2680fbfe55dd24469fa2fbdef68285f949faab96
DavidY <73976994+SAGO68plus@users.noreply.github.com>
2022-12-25 10:15:54 +0800

    module bug fixes

--- ColorSplitApp/App/static/xy-ui/components/xy-img.js
+++ ColorSplitApp/App/static/xy-ui/components/xy-img.js
@@ -41,6 +41,8 @@ class XyGallery extends HTMLElement {
             visibility:hidden;
             box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12);
             transition:.3s cubic-bezier(.645, .045, .355, 1);
+            background-color: white;
+
         }
         :host([open]) ::slotted(img){
             
